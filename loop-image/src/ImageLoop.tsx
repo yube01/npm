d868af901f98dc294f3b8411f0b6901a38c4d1cc
yube01 <yubraja008@gmail.com>
2024-10-24 10:17:15 +0545

    initial file structure for image loop pkg

---
+++ loop-image/src/ImageLoop.tsx
@@ -0,0 +1,22 @@
+
+import React, { useEffect, useState } from 'react';
+
+interface ImageLoopProps {
+    images: string[];
+    delay?: number;
+}
+
+const ImageLoop: React.FC<ImageLoopProps> = () => {
+
+
+
+    return (
+        <div>
+
+        </div >
+    );
+};
+
+
+
+export default ImageLoop;
---
+++ loop-image/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader'], // Add loaders for CSS files
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+    libraryTarget: 'commonjs2',
+  },
+};
